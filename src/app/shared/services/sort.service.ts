607b47910aa7e73a84f47919a295251e6d6d6364
AndreaPinna90 <andrea.pinna90@libero.it>
2020-01-09 17:50:52 +0100

    1) aggiunta ordina meteo in surf-component.html  2) ordinamento vento funzionante in surf-component.html

--- src/app/shared/services/sort.service.ts
+++ src/app/shared/services/sort.service.ts
@@ -25,6 +25,11 @@ export class SortService {
           this.integerCriteria(a.weather.data[0].clouds, b.weather.data[0].clouds)
         );
         break;
+      case 'wind_speed':
+          data = data.sort((a: Beach, b: Beach) =>
+            this.integerCriteria(a.weather.data[0].wind_spd, b.weather.data[0].wind_spd)
+          );
+        break;
     }
 
     return data;
